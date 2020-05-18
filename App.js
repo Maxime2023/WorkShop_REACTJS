5d2cad19fec0ac49acdad78b73814f56e76300ee
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2020-05-18 14:12:45 +0200

    Update App.js

--- App.js
+++ App.js
@@ -14,34 +14,18 @@ class App extends Component {
     films_list: [],
   }
   componentDidMount() {
-    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=4d4ed145d3584846f5922b6a467e1f85`)
-      .then(res => {
-        const films_list = res.data.results;
-        this.setState({ films_list });
-      })
+    # A completer => chargement de l'API ici #
   }
   render() {
-  const style = {width: "200px"};
   return (
     <div>
-    <div className="Header">Workshop ReactJS</div>
+    # A completer => Header #
     <Router>
       <div>
         <Switch>
         <Route path="/about" component={Description}/>
           <Route path="/">
-           <div className="flex-row-container" >
-                 {this.state.films_list.map(film => 
-                    <div className="flex-row-item" >
-                        <div className="Image">
-                            <Link to={{ pathname: "/about", film: {film}}} ><img style={style} src= {`https://image.tmdb.org/t/p/w600_and_h900_bestv2`+ film.poster_path}  alt={film.title}></img></Link>
-                        </div>
-                        <div>
-                            {film.title.slice(0, 20)}
-                        </div>
-                    </div>
-                )}
-             </div>
+          # A completer => gestion des routes #
           </Route>
         </Switch>
       </div>
@@ -50,10 +34,5 @@ class App extends Component {
   );
 }
 }
-
-// const Description = (props)=> {
-//   console.log(props)
-//   return <h2>About</h2>;
-// }
 
 export default App
