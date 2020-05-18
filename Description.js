a02201050a01e6f77f037f0213c19f697dc5e970
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2020-05-18 14:09:30 +0200

    Update Description.js

--- Description.js
+++ Description.js
@@ -4,27 +4,10 @@ import './Description.css';
 class Description extends Component {
 
     render() {
-        try {
-        
-        const {film} = this.props.location.film;
-        const style = {width: "434px"};
-        console.log(film)
+        # A completer #
         return (
-            <div className="DescriptionAll">
-                <div className="Poster">
-                    <img style={style} src= {`https://image.tmdb.org/t/p/w600_and_h900_bestv2`+ film.poster_path}  alt={film.title}></img>
-                </div>
-            <div className="Text">
-                <div>Titre: {film.title}</div>
-                <div>Langue originale: {film.original_title}</div>
-                <div>Popularité: {film.popularity}</div>
-                <div>résumé: {film.overview}</div>
-            </div>
-            </div>
+            # A completer #
         );
-        }
-        catch {
-            return <div>s</div>
         }
     }
 }
