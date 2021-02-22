c22eb098a4f98be8b781222caeb3d14a498069c9
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2021-02-22 17:22:05 +0100

    Update Description.js

--- Description.js
+++ Description.js
@@ -7,9 +7,10 @@ class Description extends Component {
     componentDidMount() {
         this.setState({filmDescription: this.props.location.film.film});
         //on met à jour la variable filmDescription, avec les props que nous envoient le composant parent (App.js)  =>this.props correspond à ce que "recoit en parametre" Description.js
+        console.log(this.state.filmDescription);
+        //permet d'afficher dans la console ce que contient this.state.filmDescription
     }
     render() {
-        console.log(this.state.filmDescription);
         return (
             <div>Description</div>
         );
