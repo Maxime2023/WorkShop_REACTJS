6f57c65f7b79dd98bd1b5b32efc97ae56fda6f88
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2021-02-22 17:11:52 +0100

    Create Description.js

---
+++ Description.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Description.css'
+class Description extends Component {
+    state = {
+        // pareil que dans app.js creer un variable d'etat qui va stocker les infos
+      }
+    componentDidMount() {
+        //Quand la classe description est appellée, il faut mettre a jour la variable d'état qui va contenir l'information
+    }
+    render() {
+        return (
+            <div>Description</div>
+        );
+    }
+}
+
+export default Description;
