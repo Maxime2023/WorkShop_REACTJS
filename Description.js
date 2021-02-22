a92f0fe1e5db42816231c42af41d4c99e103c899
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2021-02-22 17:20:36 +0100

    Update Description.js

--- Description.js
+++ Description.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 // créer le fichier Description.css import './Description.css'
 class Description extends Component {
     state = {
-        // pareil que dans app.js creer un variable d'etat qui va stocker les infos
+        filmDescription: "",
       }
     componentDidMount() {
-        //Quand la classe description est appellée, il faut mettre a jour la variable d'état qui va contenir l'information
+        this.setState({filmDescription: this.props.location.film.film});
+        //on met à jour la variable filmDescription, avec les props que nous envoient le composant parent (App.js)  =>this.props correspond à ce que "recoit en parametre" Description.js
     }
     render() {
         return (
