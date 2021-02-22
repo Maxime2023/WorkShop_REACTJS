15958a8bee49b0b0eb97a17090032abcc180b8da
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2021-02-22 17:53:02 +0100

    Update Description.js

--- Description.js
+++ Description.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-// créer le fichier Description.css import './Description.css'
+import './Description.css'
 class Description extends Component {
     state = {
         filmDescription: "",
       }
     componentDidMount() {
-        this.setState({filmDescription: this.props.location.film.film});
-        //on met à jour la variable filmDescription, avec les props que nous envoient le composant parent (App.js)  =>this.props correspond à ce que "recoit en parametre" Description.js
-        console.log(this.state.filmDescription);
-        //permet d'afficher dans la console ce que contient this.state.filmDescription
+        this.setState({filmDescription: this.props.location.film.film})
     }
     render() {
+        console.log(this.state.filmDescription)
+        const style = {width: "300px"};
         return (
             <div>
-            ///permet de verifier si filmDescription est vide ou non
                 {Object.keys(this.state.filmDescription).length === 0 ? <div>chargement</div> : 
                 <div className="Wrapper">
+                    <img style={style} src= {`https://image.tmdb.org/t/p/w600_and_h900_bestv2`+ this.state.filmDescription.poster_path}  alt={this.state.filmDescription.title}></img>
                     <div><h1>Titre :</h1> {this.state.filmDescription.original_title}</div>
+                    <div><h1>Description : </h1>{this.state.filmDescription.overview}</div>
+                    <div><h1>Date de sortie : </h1>{this.state.filmDescription.release_date}</div>
+                    <div><h1>Popularité :</h1>{this.state.filmDescription.popularity}</div>
+                    
                 </div>
                 }
             </div>
+
         );
     }
 }
